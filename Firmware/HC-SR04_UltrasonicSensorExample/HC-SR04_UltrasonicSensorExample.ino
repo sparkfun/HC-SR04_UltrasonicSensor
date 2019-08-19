53ae221e3b46cdba0c72f0801f25e3b726319faa
Ho Yun "Bobby <ho.chan@colorado.edu>
2019-08-19 11:58:38 -0600

    Update HC-SR04_UltrasonicSensorExample.ino
    Set  echo pin as input in setup to ensure that the pin is set correctly

--- Firmware/HC-SR04_UltrasonicSensorExample/HC-SR04_UltrasonicSensorExample.ino
+++ Firmware/HC-SR04_UltrasonicSensorExample/HC-SR04_UltrasonicSensorExample.ino
@@ -1,25 +1,25 @@
 /**
- * HC-SR04 Demo
- * Demonstration of the HC-SR04 Ultrasonic Sensor
- * Date: August 3, 2016
- * 
- * Description:
- *  Connect the ultrasonic sensor to the Arduino as per the
- *  hardware connections below. Run the sketch and open a serial
- *  monitor. The distance read from the sensor will be displayed
- *  in centimeters and inches.
- * 
- * Hardware Connections:
- *  Arduino | HC-SR04 
- *  -------------------
- *    5V    |   VCC     
- *    7     |   Trig     
- *    8     |   Echo     
- *    GND   |   GND
- *  
- * License:
- *  Public Domain
- */
+   HC-SR04 Demo
+   Demonstration of the HC-SR04 Ultrasonic Sensor
+   Date: August 3, 2016
+
+   Description:
+    Connect the ultrasonic sensor to the Arduino as per the
+    hardware connections below. Run the sketch and open a serial
+    monitor. The distance read from the sensor will be displayed
+    in centimeters and inches.
+
+   Hardware Connections:
+    Arduino | HC-SR04
+    -------------------
+      5V    |   VCC
+      7     |   Trig
+      8     |   Echo
+      GND   |   GND
+
+   License:
+    Public Domain
+*/
 
 // Pins
 const int TRIG_PIN = 7;
@@ -33,6 +33,10 @@ void setup() {
   // The Trigger pin will tell the sensor to range find
   pinMode(TRIG_PIN, OUTPUT);
   digitalWrite(TRIG_PIN, LOW);
+
+  //Set Echo pin as input to measure the duration of 
+  //pulses coming back from the distance sensor
+  pinMode(ECHO_PIN, INPUT);
 
   // We'll use the serial monitor to view the sensor output
   Serial.begin(9600);
@@ -62,7 +66,7 @@ void loop() {
   pulse_width = t2 - t1;
 
   // Calculate distance in centimeters and inches. The constants
-  // are found in the datasheet, and calculated from the assumed speed 
+  // are found in the datasheet, and calculated from the assumed speed
   //of sound in air at sea level (~340 m/s).
   cm = pulse_width / 58.0;
   inches = pulse_width / 148.0;
@@ -76,7 +80,7 @@ void loop() {
     Serial.print(inches);
     Serial.println(" in");
   }
-  
+
   // Wait at least 60ms before next measurement
   delay(60);
 }
